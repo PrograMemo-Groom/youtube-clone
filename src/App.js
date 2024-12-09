cadca5f787893217f6dac4da7ba8e15bb81c7759
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 23:20:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {Outlet, Route, Routes} from "react-router-dom";
+import React from "react";
+import MainPage from "./components/main/Main";
+import styles from "./App.module.css";
+
+const LayOut = () => {
+  return (
+      <>
+        {/*<Header />*/}
+        {/*<Nav />*/}
+        <div className={styles.container}>
+          <Outlet /> {/* 현재 라우터에 따라 변경 되는 내용 */}
+        </div>
+        {/*<Footer />*/}
+      </>
+  )
+}
+
+function App() {
+  console.log('App is running!');
+  return (
+    <div className={styles.app}>
+      <Routes>
+        <Route path="/" element={<LayOut />}>
+          <Route index element={<MainPage />} />
+          {/*<Route path="search" element={<SearchPage />} />*/}
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+const Main = () => {
+    return (
+        <div className={styles.container}>
+            MainPage
+        </div>
+    );
+};
+
+export default Main;
