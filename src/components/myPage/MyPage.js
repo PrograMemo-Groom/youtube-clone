4073119caa33ada2b5be6c44fc3832cb30a0bd42
jdaily00 <kyeonghee_@naver.com>
2024-12-10 14:35:57 +0900

    ✨ 내 페이지 사용자 프로필 및 계정 보기

--- src/components/myPage/MyPage.js
+++ src/components/myPage/MyPage.js
@@ -8,6 +8,15 @@ export default function MyPage() {
                 <img className="user-profile-img"
                      src="/assets/mypage/user-profile.png"
                      alt="user-profile-img"/>
+                <div className="user-name-and-id-container">
+                    <section className="user-name-container">
+                        <p className="user-name">공공</p>
+                        <a href="#" className="user-channel-move">@o0_o0_o0 &#183; 채널 보기</a>
+                    </section>
+                    <section className="changes-id">
+
+                    </section>
+                </div>
             </div>
         </div>
     )
