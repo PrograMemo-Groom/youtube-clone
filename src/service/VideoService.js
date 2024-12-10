2d5c1eb0e61966a6c147da2df1f817d6fd7b7538
Jae-Hong-Kim <newfly101@naver.com>
2024-12-10 11:48:16 +0900

    💡 youtube api : params 설명 주석 추가

--- src/service/VideoService.js
+++ src/service/VideoService.js
@@ -6,11 +6,22 @@ export const fetchPopularVideos = async () => {
     try{
         const {data: {items : response}} = await instance.get(requests.fetchPopularVideos, {
             params: {
-                part: "snippet,statistics",
+                part: "snippet,statistics,player",
                 chart: "mostPopular",
                 regionCode: "KR",
                 maxResults: 10,
             }
+
+            /* part: 가져올 데이터의 종류 (예: snippet, statistics, contentDetails 등).
+            - snippet: 동영상의 기본 메타데이터. (제목(title), 설명(description), 채널 정보(channelTitle), 썸네일(thumbnail) 등이 포함)
+            - statistics: 동영상의 통계 데이터. (조회수(viewCount), 좋아요 수(likeCount), 댓글 수(commentCount) 등을 제공)
+            - contentDetails: 동영상의 상세 정보. (동영상 길이(duration), 콘텐츠 등급(contentRating) 등이 포함)
+            - player: 동영상을 삽입(embed)할 수 있는 HTML 코드.
+            - status: 동영상의 상태. (공개 상태(public/private), 라이선스 정보 등이 포함)
+             */
+            // chart: mostPopular를 설정.
+            // regionCode: 국가 코드 (예: KR, US, JP).
+            // maxResults: 한번의 요청에서 결과 개수 (기본: 5, 최대: 50).
         });
         console.log(tag, "fetchPopularVideos response",response);
         return response;
