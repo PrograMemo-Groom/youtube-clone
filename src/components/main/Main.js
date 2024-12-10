9a5bbd8444a955fcee9810c4eff0dcc8b4651e4f
Jae-Hong-Kim <newfly101@naver.com>
2024-12-10 11:49:03 +0900

    ✨ main에서 인기있는 data 가져오는 부분 설정

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Main.module.css';
+import {fetchPopularVideos} from "../../service/VideoService";
 
+// const tag = '[Main]';
 const Main = () => {
+    const [video, setVideo] = useState(null);
+    // 화면 최초 렌더링 시 video를 가져옴
+    useEffect(() => {
+        // 인기있는 video를 가져오는 부분
+        setVideo(fetchPopularVideos());
+    }, []);
+
     return (
         <div className={styles.container}>
-            MainPage
+            1
         </div>
     );
 };
