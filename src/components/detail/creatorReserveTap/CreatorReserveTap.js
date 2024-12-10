7fc83071ac67e8f3621255b7aac0cd0849e9e962
saaad99 <ehdgnstla@naver.com>
2024-12-10 16:56:50 +0900

    💄creator-reserve-tap의 ui구조, 스타일링 완료

--- src/components/detail/creatorReserveTap/CreatorReserveTap.js
+++ src/components/detail/creatorReserveTap/CreatorReserveTap.js
@@ -4,29 +4,31 @@ import "./CreatorReserveTap.css";
 function CreatorReserveTap() {
   return (
     <section className='creator-reserve-container'>
-        <div className='menu-section'>메뉴섹션
-            <img className='left-arrow' src='' alt='왼쪽 화살표'/>
-            <div className='menu-list'>메뉴 리스트
-                <div className='menu-item'>메뉴1</div>
-                <div className='menu-item'>메뉴2</div>
+        <div className='menu-section'>
+            <span className='left-arrow'>{'<'}</span>
+            <div className='menu-list'>
+                <div className='menu-item'>모두</div>
+                <div className='menu-item'>시리즈</div>
+                <div className='menu-item'>blue rain 제공</div>
+                <div className='menu-item'>관련 콘텐츠</div>
                 <div className='menu-item'>메뉴3</div>
                 <div className='menu-item'>메뉴4</div>
             </div>
-            <img className='right-arrow' src='' alt='오른쪽 화살표'/>
+            <span className='right-arrow'>{'>'}</span>
         </div>
-        <div className='video-section'>비디오 섹션
+        <div className='video-section'>
             <div className='video-box'>
-                <img className='video' src='' alt='영상 썸네일'/>
+                <span className='video'></span>
                 <span className='time-stamp'>1:13:41</span>
             </div>
-            <div className='video-detail'>
+            <div className='video-details'>
                 <div className='video-title'>잠잘 때, 작업할 때 듣기좋은 시간대별 BGM 모음</div>
                 <div className='channel-name'>by. 채널명</div>
                 <div className='video-info'>
                     <span className='viewer-count'>조회수 116만회</span>
-                    <span className='upload-date'>4년전</span>
+                    <span className='upload-date'> 4년전</span>
                 </div>
-                <img className='more-btn' src='' alt='영상 더보기'/>
+                <img className='more-btn' src='assets/icon/more_btn.svg' alt='영상 더보기'/>
             </div>
         </div>
         
