8a86dd5bce36d27d23c1970437d39cd6416c72c6
Jae-Hong-Kim <newfly101@naver.com>
2024-12-10 10:57:19 +0900

    💄 Sidebar 추가 및 rendering page css 조정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 import MainPage from "./components/main/Main";
 import styles from "./App.module.css";
 import Header from "./components/header/Header";
+import SideBar from "./components/sidebar/Sidebar";
 
 const LayOut = () => {
   return (
       <>
         <Header />
-        {/*<Nav />*/}
+        <SideBar />
         <div className={styles.container}>
           <Outlet /> {/* 현재 라우터에 따라 변경 되는 내용 */}
         </div>
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Sidebar.module.css";
+
+const Sidebar = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+};
+
+export default Sidebar;
