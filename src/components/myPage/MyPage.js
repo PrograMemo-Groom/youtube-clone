f31e5254740fea5faecd0f099082b82257a301f9
jdaily00 <kyeonghee_@naver.com>
2024-12-10 13:51:28 +0900

    ✨ 내 페이지 컨테이너 위치 잡기

---
+++ src/components/myPage/MyPage.js
@@ -0,0 +1,12 @@
+import "./MyPage.css";
+
+export default function MyPage() {
+    console.log("MyPage is rendering!");
+    return (
+        <div className="contents-container">
+            <div className="user-page-info-container">
+
+            </div>
+        </div>
+    )
+}
