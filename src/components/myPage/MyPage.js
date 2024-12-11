c9a631e6cad3db4967268d58f955c76b99c14f09
jdaily00 <kyeonghee_@naver.com>
2024-12-11 16:17:53 +0900

    ✨ 내 페이지: 시청기록 임의의 데이터 생성 (videoId를 사용해서 동적으로 썸네일 URL 생성함)

--- src/components/myPage/MyPage.js
+++ src/components/myPage/MyPage.js
@@ -1,5 +1,33 @@
 import "./MyPage.css";
 
+const videoData = [{
+    videoId: "8yEzRxsilu0",
+    title: "[KPOP Playlist] 요즘 듣는 케이팝 노동요 플레이리스트]",
+    channel: "김로라",
+    view: "7.3만회",
+    uploadedAt: "4주 전",
+    duration: "3:13:00",
+}, {
+    videoId: "VkHkoSk2Xos",
+    title: "12월 케이팝 여자아이돌 걸그룹 노래모음 (가사포함) | 플레이리스트",
+    channel: "밤공원",
+    view: "6.7만회",
+    uploadedAt: "4일 전",
+    duration: "2:01:50",
+}]
+
+const thumbnailUrl = (videoId) => {
+    return `https://i.ytim.com/vi/${videoId}/mqdefault.jpg`;
+}
+
+// videoId를 map으로 돌려서 썸네일 Url 추가
+const updatethumbnailUrl = videoData.map((video) => ({
+    ...video,
+    thumbnail: thumbnailUrl(video.videoId), // 썸네일 URL 생성
+}))
+
+console.log(updatethumbnailUrl);
+
 export default function MyPage() {
     console.log("MyPage is rendering!");
     return (
@@ -21,12 +49,12 @@ export default function MyPage() {
                             <p className="changes-id-text">계정 전환</p>
                         </section>
                         <div className="Google-id-change-container">
-                        <section className="Google-id-container">
-                            <img className="chang-id-icon"
-                                 src="/assets/mypage/google-logo-icon.svg"
-                                 alt="google-id"/>
-                            <p className="changes-id-text">Google 계정</p>
-                        </section>
+                            <section className="Google-id-container">
+                                <img className="chang-id-icon"
+                                     src="/assets/mypage/google-logo-icon.svg"
+                                     alt="google-id"/>
+                                <p className="changes-id-text">Google 계정</p>
+                            </section>
                         </div>
                     </div>
                 </div>
