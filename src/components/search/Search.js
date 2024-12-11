4bb420bfe5c088d1fe056594f67a3079fca8e90c
Jae-Hong-Kim <newfly101@naver.com>
2024-12-12 00:58:25 +0900

    💄 search video ui 구성 및 css 조정

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -14,21 +14,64 @@ const Search = () => {
     const searchTerm = query.get("q");
 
     useEffect(() => {
-        if (searchTerm.trim().length > 0) {
-            setSearchResult(fetchSearchVideos(searchTerm).items);
-            console.log("여기");
-            setNextToken(searchResult.pageToken);
+        const fetchData = async () => {
+            if (searchTerm.trim().length > 0) {
+                const response = await fetchSearchVideos(searchTerm);
+                console.log(tag, "fetchData:", response);
+                setSearchResult(response.items || []);
+                setNextToken(response.pageToken || "");
+            }
         }
-    }, [fetchSearchVideos]);
+        fetchData();
+    }, [searchTerm]);
 
     return (
-        <div>
-            <div>{nextToken}</div>
+        <div className={styles.container}>
+            <div className={styles.categoryWrapper}>
+                <div className={styles.categories}>
+                    <button>전체</button>
+                    <button>shorts</button>
+                    <button>동영상</button>
+                    <button>시청하지 않음</button>
+                    <button>감상한 동영상</button>
+                    <button>최근에 업로드된 동영상</button>
+                    <button>라이브</button>
+                    <button>채널</button>
+                </div>
+            </div>
+            <div className={styles.videos}>
+                {searchResult && searchResult.map((video) => {
+                    // console.log(video);
+                    // const thisData = new Date();
+                    // console.log(today)
+                    return (
+                        <div className={styles.videoLists}>
+                            <div className={styles.videoFrame}>
+                                <iframe
+                                    src={`https://www.youtube-nocookie.com/embed/${video.videoId}?controls=0&autoplay=1&loop=1&mute=1&playlist=${video.videoId}`}
+                                    title={video.title}
+                                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-storage-access-by-user-activation"
+                                    // loading="lazy"
+                                />
+                                <span>1:10:13</span>
+                            </div>
+                            <div className={styles.videoDescriptions}>
+                                <h2>{video.title}</h2>
+                                <p>퍼스널 Personal•조회수 1.7만회</p>
+                                <div className={styles.videoChannelInfo}>
+                                    <div className={styles.channelInfo}>
+                                        <img src={video.defaultThumbnail.url} alt="img" />
+                                        <span>{video.channelTitle}</span>
+                                    </div>
+                                </div>
+                                <p className={styles.videoDescript}>{video.description}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+                비디오
 
-            <p>{nextToken && nextToken}</p>
-            {searchResult?.map((item) => {
-                <p>{item.title}</p>
-            })}
+            </div>
         </div>
     );
 };
