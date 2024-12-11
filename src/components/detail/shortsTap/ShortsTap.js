92e8c7b4bc040756e33f47356581910fac4ffbda
saaad99 <ehdgnstla@naver.com>
2024-12-11 20:59:50 +0900

    💄 Shorts,크리에이터 제공  탭 css 수정, 서로 간섭있었음

--- src/components/detail/shortsTap/ShortsTap.js
+++ src/components/detail/shortsTap/ShortsTap.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import formatViewerCount from "../../../utils/formatViewerCount";
 import "./ShortsTap.css";
 
@@ -12,35 +12,48 @@ function ShortsTap() {
         "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
     },
     {
-        id: 2,
-        title: "모동숲 새해 카운트 다운",
-        viewerCount: 540000,
-        thumbUrl:
-          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
-      },
-      {
-        id: 3,
-        title: "모동숲 새해 카운트 다운",
-        viewerCount: 231,
-        thumbUrl:
-          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
-      },
-      {
-        id: 4,
-        title: "모동숲 새해 카운트 다운",
-        viewerCount: 123,
-        thumbUrl:
-          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
-      },
-      {
-        id: 5,
-        title: "모동숲 새해 카운트 다운",
-        viewerCount: 540000,
-        thumbUrl:
-          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
-      },
+      id: 2,
+      title: "모동숲 새해 카운트 다운",
+      viewerCount: 540000,
+      thumbUrl:
+        "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+    },
+    {
+      id: 3,
+      title: "모동숲 새해 카운트 다운",
+      viewerCount: 231,
+      thumbUrl:
+        "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+    },
+    {
+      id: 4,
+      title: "모동숲 새해 카운트 다운",
+      viewerCount: 123,
+      thumbUrl:
+        "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+    },
+    {
+      id: 5,
+      title: "모동숲 새해 카운트 다운",
+      viewerCount: 540000,
+      thumbUrl:
+        "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+    },
   ]);
+  // 스크롤 이벤트를 위한 Ref
+  const categoryBarRef = useRef(null);
 
+  // 스크롤 이벤트
+  const handleScroll = (direction) => {
+    const scrollContainer = categoryBarRef.current;
+    // console.log(scrollContainer);
+    const scrollAmount = scrollContainer.clientWidth;
+    if (direction === "left") {
+      scrollContainer.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    } else if (direction === "right") {
+      scrollContainer.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  };
 
   return (
     <div className='shortsTap-container'>
@@ -53,11 +66,16 @@ function ShortsTap() {
         <span>Shorts</span>
       </div>
 
-      <div className='shorts-list'>
-        {/* <div className="left-arrow">{"<"}</div>
-        <div className="right-arrow">{">"}</div> */}
+      <div className='left-arrow' onClick={() => handleScroll("left")}>
+          {"<"}
+        </div>
+        <div className='right-arrow' onClick={() => handleScroll("right")}>
+          {">"}
+        </div>
+
+      <div className='shorts-list' ref={categoryBarRef}>
         {shorts.map((short, index) => (
-          <figure className="shorts-item" key={index}>
+          <figure className='shorts-item' key={index}>
             <img
               className='shorts-item-img'
               src={short.thumbUrl}
@@ -65,7 +83,9 @@ function ShortsTap() {
             />
             <div className='shorts-item-info'>
               <span className='title'>{short.title}</span>
-              <span className='viewer-count'>조회수 {formatViewerCount(short.viewerCount)}</span>
+              <span className='viewer-count'>
+                조회수 {formatViewerCount(short.viewerCount)}
+              </span>
             </div>
           </figure>
         ))}
