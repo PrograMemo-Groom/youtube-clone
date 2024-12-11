7e4480ab799abc646b95e6368f5a7a1c5a84a77f
Jae-Hong-Kim <newfly101@naver.com>
2024-12-11 16:24:32 +0900

    ➕ .env에 google_Oauth url/key 추가

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,4 @@
 export const API_URL = process.env.REACT_APP_YOUTUBE_API_URL;
 export const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+export const API_OAUTH_URL = process.env.REACT_APP_YOUTUBE_OAUTH_URL;
+export const API_OAUTH_KEY = process.env.REACT_APP_YOUTUBE_OAUTH_KEY;
