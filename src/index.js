f1d89642369446bc2c50f65b4f0608553e92fa92
Jae-Hong-Kim <newfly101@naver.com>
2024-12-11 17:07:19 +0900

    🎨 [구독] 구글 인증을 위한 client_id를 제공하는 provider 추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {GoogleOAuthProvider} from "@react-oauth/google";
+import {API_OAUTH_KEY} from "./config/config";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={API_OAUTH_KEY}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </GoogleOAuthProvider>
 );
