2bb5ae45897d6ae24cd3cc5e9fd762d656d5b942
Jae-Hong-Kim <newfly101@naver.com>
2024-12-11 19:06:55 +0900

    🎨 [구독] accessToken 발급하게 변경 -> localStorage에 데이터 저장

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -4,17 +4,17 @@ import {GOOGLE_OAUTH_URL} from "../config/config";
 const useGoogleAuth = () => {
     const login = useGoogleLogin({
         onSuccess: (response) => {
-            console.log("Client Login Access Token:", response);
-            return response.code;
+            console.log("Client Login Authorization Code:", response);
+            localStorage.setItem("GOOGLE_TOKEN", response.access_token);
+            return Promise.resolve(response.access_token);
         },
         onError: (error) => {
-            console.error("Client Login Failed:", error);
+            console.error("Google Login Failed:", error);
+            return Promise.reject(error);
         },
         scope: GOOGLE_OAUTH_URL,
-        flow: "auth-code",
     });
     return login;
 };
 
 export default useGoogleAuth;
-
