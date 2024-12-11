2a385732187add4eb31e9ee09514e14f682ecb98
yoorym-kim <twilight99me@gmail.com>
2024-12-11 16:16:36 +0900

    🔧service 파일 추가, api 호출부분 이동

--- src/components/subscribe/Manage-subscribe.js
+++ src/components/subscribe/Manage-subscribe.js
@@ -1,55 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Manage-subscribe.module.css';
-import instance from '../../api/api';
-import requests from '../../api/endpoint';
+import {fetchSubscriptions} from "./service";
 
-const tag = '[Service]';
-const fetchSubscriptions = async () => {
-    try {
-        //구독 목록 가져오기
-        const {data: {items}} = await instance.get(requests.fetchSubscriptions, {
-            params: {
-                part: "snippet",
-                mine: "true",
-                maxResults: "10"  //변경하기 !
-
-            }
-        });
-
-        //아이디 넘겨서 체널 디테일 가져오기
-        const channelDetails = await Promise.all(
-            items.map(item => fetchChannelDetails(item.snippet.resourceId.channelId))
-        );
-
-        return channelDetails.filter(Boolean);
-
-    } catch (e) {
-        console.log(tag, "Subscriptions can't get response data", e);
-    }
-}
-
-const fetchChannelDetails = async (channelId) => {
-    try {
-        //채널 아이디에 따른 디테일 가져오기
-        const {data} = await instance.get(requests.fetchChannelDetails, {
-            params: {
-                part: 'statistics,snippet',
-                id: channelId,
-            }
-        })
-        const channel = data.items[0];
-        return {
-            id: channel.id,
-            title: channel.snippet.title,
-            description: channel.snippet.description,
-            profileImage: channel.snippet.thumbnails.default.url,
-            subscriberCount: channel.statistics.subscriberCount,
-        }
-    } catch (e) {
-        console.log(tag, "Error fetching channel details", e);
-        return null;
-    }
-}
 
 const ManageSubscribe = () => {
     const [subscriptions, setSubscriptions] = useState([]);
