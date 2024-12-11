0352f5ce3d2e860d6701cbdc4ed9e3716fe929b0
happygirlll <lbm000314@gmail.com>
2024-12-11 16:13:30 +0900

    🎨[sidebar] Link 대신 React Router의 useNavigate hook을 사용

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import styles from "./Sidebar.module.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.container}>
-            <Link to="/" className={styles.sidebar_Item}>
-                <img src={`${process.env.PUBLIC_URL}/home.png`} alt="홈" className={styles.sidebar_icon} />
+            <div
+                className={styles.sidebar_Item}
+                onClick={() => navigate('/')}
+            >
+                <img src={`${process.env.PUBLIC_URL}/home.png`} alt="홈" className={styles.sidebar_icon}/>
                 <span>홈</span>
-            </Link>
-            <Link to="/shorts" className={styles.sidebar_Item}>
-                <img src={`${process.env.PUBLIC_URL}/shorts.png`} alt="Shorts" className={styles.sidebar_icon} />
+            </div>
+            <div
+                className={styles.sidebar_Item}
+                onClick={() => navigate('/shorts')}
+            >
+                <img src={`${process.env.PUBLIC_URL}/shorts.png`} alt="Shorts" className={styles.sidebar_icon}/>
                 <span>Shorts</span>
-            </Link>
-            <Link to="/subscriptions" className={styles.sidebar_Item}>
-                <img src={`${process.env.PUBLIC_URL}/subscription.png`} alt="구독" className={styles.sidebar_icon} />
+            </div>
+            <div
+                className={styles.sidebar_Item}
+                onClick={() => navigate('/subscribe')}
+            >
+                <img src={`${process.env.PUBLIC_URL}/subscription.png`} alt="구독" className={styles.sidebar_icon}/>
                 <span>구독</span>
-            </Link>
-            <Link to="/my-page" className={styles.sidebar_Item}>
-                <img src={`${process.env.PUBLIC_URL}/mypage.png`} alt="내 페이지" className={styles.sidebar_icon} />
+            </div>
+            <div
+                className={styles.sidebar_Item}
+                onClick={() => navigate('/myPage')}
+            >
+                <img src={`${process.env.PUBLIC_URL}/mypage.png`} alt="내 페이지" className={styles.sidebar_icon}/>
                 <span>내 페이지</span>
-            </Link>
+            </div>
         </div>
     );
 };
