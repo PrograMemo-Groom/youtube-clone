1164d1c0b7c9c4c7b3a175e08e9be0a58a4d659e
S2ej1n <ks320205@naver.com>
2024-12-12 04:38:21 +0900

    💄 오른쪽 panel창 css

--- src/components/shorts/Panel.js
+++ src/components/shorts/Panel.js
@@ -4,7 +4,18 @@ import styles from './Panel.module.css';
 const Panel = () => {
   return (
     <div className={styles.Panel_Container}>
-        히히 댓글창 재사용할거지롱
+        <div className={styles.contents_container}>
+          <header className={styles.contents_header}>
+            <p>설명</p>
+            <button>X</button>
+          </header>
+          <section className={styles.contents_section}>
+            <p>한국과 전혀 다른 트와이스 미국 인기 반응</p>
+          </section>
+          <section className={styles.contents_factoid}>
+            ddd
+          </section>
+        </div>
     </div>
   )
 }
