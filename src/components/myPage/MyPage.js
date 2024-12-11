1027eea578c8cda3653d445726f192762f27c0aa
jdaily00 <kyeonghee_@naver.com>
2024-12-11 18:22:02 +0900

    ✨ 내 페이지: 임의의 데이터로 시청기록 동영상 및 영상 정보 배치

--- src/components/myPage/MyPage.js
+++ src/components/myPage/MyPage.js
@@ -2,6 +2,7 @@ import "./MyPage.css";
 
 const videoData = [{
     videoId: "8yEzRxsilu0",
+    thumbnail: "https://i.ytimg.com/vi/Gg_J9Eonl4Q/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&amp;rs=AOn4CLAag4fMJqf99yAtvLDQNbu2ZJ94Lw",
     title: "[KPOP Playlist] 요즘 듣는 케이팝 노동요 플레이리스트]",
     channel: "김로라",
     view: "7.3만회",
@@ -9,21 +10,51 @@ const videoData = [{
     duration: "3:13:00",
 }, {
     videoId: "VkHkoSk2Xos",
-    title: "12월 케이팝 여자아이돌 걸그룹 노래모음 (가사포함) | 플레이리스트",
+    title: "11월 케이팝 여자아이돌 걸그룹 노래모음 (가사포함) | 플레이리스트 | Playlist | Kpop",
+    thumbnail: "https://i.ytimg.com/vi/xFwNdsuoseQ/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLBIiv2i0UwmAX5ch9qIAWpQ0m-4yA",
     channel: "밤공원",
     view: "6.7만회",
     uploadedAt: "4일 전",
     duration: "2:01:50",
-}]
+},
+    {
+        videoId: "8yEzRxsilu0",
+        thumbnail: "https://i.ytimg.com/vi/Gg_J9Eonl4Q/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&amp;rs=AOn4CLAag4fMJqf99yAtvLDQNbu2ZJ94Lw",
+        title: "[KPOP Playlist] 요즘 듣는 케이팝 노동요 플레이리스트]",
+        channel: "김로라",
+        view: "7.3만회",
+        uploadedAt: "4주 전",
+        duration: "3:13:00",
+    }, {
+        videoId: "VkHkoSk2Xos",
+        title: "11월 케이팝 여자아이돌 걸그룹 노래모음 (가사포함) | 플레이리스트 | Playlist | Kpop",
+        thumbnail: "https://i.ytimg.com/vi/xFwNdsuoseQ/hqdefault.jpg?s…BACGAY4AUAB&rs=AOn4CLBIiv2i0UwmAX5ch9qIAWpQ0m-4yA",
+        channel: "밤공원",
+        view: "6.7만회",
+        uploadedAt: "4일 전",
+        duration: "2:01:50",
+    },
+    {
+        videoId: "8yEzRxsilu0",
+        thumbnail: "https://i.ytimg.com/vi/Gg_J9Eonl4Q/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&amp;rs=AOn4CLAag4fMJqf99yAtvLDQNbu2ZJ94Lw",
+        title: "[KPOP Playlist] 요즘 듣는 케이팝 노동요 플레이리스트]",
+        channel: "김로라",
+        view: "7.3만회",
+        uploadedAt: "4주 전",
+        duration: "3:13:00",
+    },
+]
 
-const thumbnailUrl = (videoId) => {
-    return `https://i.ytim.com/vi/${videoId}/mqdefault.jpg`;
-}
+
+// const thumbnailUrl = (videoId) => {
+//     return `https://i.ytim.com/vi/${videoId}/mqdefault.jpg`;
+// }
 
 // videoId를 map으로 돌려서 썸네일 Url 추가
 const updatethumbnailUrl = videoData.map((video) => ({
     ...video,
-    thumbnail: thumbnailUrl(video.videoId), // 썸네일 URL 생성
+    // thumbnail: thumbnailUrl(video.videoId), // 썸네일 URL 생성
+    thumbnail: `https://i.ytim.com/vi/${video.videoId}/mqdefault.jpg`,
 }))
 
 console.log(updatethumbnailUrl);
@@ -68,12 +99,14 @@ export default function MyPage() {
                     <section className="video-list">
                         {videoData.map((video) => (
                             <section className="video-item" key={video.videoId}>
-                                <img
-                                    className="video-thumbnail"
-                                    src={thumbnailUrl(video.videoId)}
-                                    alt={video.title}
-                                />
-                                <div className="video-info">
+                                <div className="video-thumbnail-container">
+                                    <img
+                                        className="video-thumbnail"
+                                        src={video.thumbnail}
+                                        alt={video.title}
+                                    />
+                                </div>
+                                <div className="video-info-container">
                                     <h3 className="video-title">{video.title}</h3>
                                     <p className="video-channel">{video.channel}</p>
                                     <p className="video-meta">
@@ -83,6 +116,26 @@ export default function MyPage() {
                             </section>
                         ))}
                     </section>
+
+                    {/*<section className="video-list">*/}
+                    {/*    {videoData.map((video) => (*/}
+                    {/*        <section className="video-item" key={video.videoId}>*/}
+                    {/*            <img*/}
+                    {/*                className="video-thumbnail"*/}
+                    {/*                // src={thumbnailUrl(video.videoId)}*/}
+                    {/*                src={video.thumbnail}*/}
+                    {/*                alt={video.title}*/}
+                    {/*            />*/}
+                    {/*            <div className="video-info">*/}
+                    {/*                <h3 className="video-title">{video.title}</h3>*/}
+                    {/*                <p className="video-channel">{video.channel}</p>*/}
+                    {/*                <p className="video-meta">*/}
+                    {/*                    {video.view} · {video.uploadedAt}*/}
+                    {/*                </p>*/}
+                    {/*            </div>*/}
+                    {/*        </section>*/}
+                    {/*    ))}*/}
+                    {/*</section>*/}
                 </section>
 
             </div>
