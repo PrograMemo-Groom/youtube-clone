4848b9322d020a4517e6db663023a85377c8c9ad
yoorym-kim <twilight99me@gmail.com>
2024-12-11 16:55:03 +0900

    ✨구독 동영상 fetch코드 추가

--- src/components/subscribe/service.js
+++ src/components/subscribe/service.js
@@ -16,7 +16,12 @@ export const fetchSubscriptions = async () => {
 
         //아이디 넘겨서 체널 디테일 가져오기
         const channelDetails = await Promise.all(
-            items.map(item => fetchChannelDetails(item.snippet.resourceId.channelId))
+            items.map(async (item) => {
+                const channelId = item.snippet.resourceId.channelId;
+                const videos = await fetchChannelVideos(channelId);  // 최신 동영상 가져오기
+                const channel = await fetchChannelDetails(channelId); // 채널 정보 가져오기
+                return { channel, videos };
+            })
         );
 
         return channelDetails.filter(Boolean);
@@ -46,5 +51,34 @@ export const fetchChannelDetails = async (channelId) => {
     } catch (e) {
         console.log(tag, "Error fetching channel details", e);
         return null;
+    }
+}
+
+// 채널 아이디로 체널 동영상 가져오기
+export const fetchChannelVideos = async (channelId) => {
+    try {
+        const { data: { items } } = await instance.get(requests.fetchVideos, {
+            params: {
+                part: 'snippet,statistics',
+                channelId: channelId,
+                order: 'date',  // !!!!!! 최신순으로 정렬 !!!!!,, 근데 체널별로 나올듯, 확인하기
+                maxResults: 10,  // 일단 10개만 가져와볼가
+            }
+        });
+        
+        // 동영상 정보 추출
+        const videos = items.map(item => ({
+            videoId: item.id.videoId,
+            title: item.snippet.title,
+            thumbnail: item.snippet.thumbnails.high.url,
+            views: item.statistics.viewCount,
+            publishedAt: item.snippet.publishedAt,
+        }));
+
+        return videos;
+
+    } catch (e) {
+        console.log(tag, `Error fetching videos for channel ${channelId}`, e);
+        return [];
     }
 }
