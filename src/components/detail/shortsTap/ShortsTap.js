b26cd178c6cee8901e9a1f0a328407c0a3fe7247
saaad99 <ehdgnstla@naver.com>
2024-12-11 19:06:17 +0900

    쇼츠탭 더미데이터로 리스트

--- src/components/detail/shortsTap/ShortsTap.js
+++ src/components/detail/shortsTap/ShortsTap.js
@@ -1,50 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import formatViewerCount from "../../../utils/formatViewerCount";
 import "./ShortsTap.css";
 
 function ShortsTap() {
+  const [shorts, setShorts] = useState([
+    {
+      id: 1,
+      title: "모동숲 새해 카운트 다운",
+      viewerCount: 123,
+      thumbUrl:
+        "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+    },
+    {
+        id: 2,
+        title: "모동숲 새해 카운트 다운",
+        viewerCount: 540000,
+        thumbUrl:
+          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+      },
+      {
+        id: 3,
+        title: "모동숲 새해 카운트 다운",
+        viewerCount: 231,
+        thumbUrl:
+          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+      },
+      {
+        id: 4,
+        title: "모동숲 새해 카운트 다운",
+        viewerCount: 123,
+        thumbUrl:
+          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+      },
+      {
+        id: 5,
+        title: "모동숲 새해 카운트 다운",
+        viewerCount: 540000,
+        thumbUrl:
+          "https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA",
+      },
+  ]);
+
+
   return (
     <div className='shortsTap-container'>
       <div className='shorts-header'>
-        <img className='shorts-header-img' src='https://i.namu.wiki/i/LkKUWu-wSjxhbv9Nc1TeDv7cQVGAm9fb51bMED8S73h0495WomoGvYlS2pbfJAx14IEeHDyMJO85nKL2Q5cs4qgVW458GNv1F3aEghP0Pgb-iDX50RJI_rhRxsOpOCJqWmnEZk-3psmR7uYQu4Kzag.svg' alt='���� 이미지' />
+        <img
+          className='shorts-header-img'
+          src='https://i.namu.wiki/i/LkKUWu-wSjxhbv9Nc1TeDv7cQVGAm9fb51bMED8S73h0495WomoGvYlS2pbfJAx14IEeHDyMJO85nKL2Q5cs4qgVW458GNv1F3aEghP0Pgb-iDX50RJI_rhRxsOpOCJqWmnEZk-3psmR7uYQu4Kzag.svg'
+          alt='쇼츠 아이콘'
+        />
         <span>Shorts</span>
       </div>
 
       <div className='shorts-list'>
         {/* <div className="left-arrow">{"<"}</div>
         <div className="right-arrow">{">"}</div> */}
-
-        <figure className='shorts-item'>
-          <img className='shorts-item-img' src='https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA' alt='미리보기 이미지' />
-          <div className='shorts-item-info'>
-          <span className="title">모동숲 새해 카운트 다운</span>
-            <span className="viewer-count">조회수 54만회</span>
-          </div>
-        </figure>
-
-        <figure className='shorts-item'>
-          <img className='shorts-item-img' src='https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA' alt='미리보기 이미지' />
-          <div className='shorts-item-info'>
-            <span className="title">모동숲 새해 카운트 다운</span>
-            <span className="viewer-count">조회수 54만회</span>
-          </div>
-        </figure>
-        
-        <figure className='shorts-item'>
-          <img className='shorts-item-img' src='https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA' alt='미리보기 이미지' />
-          <div className='shorts-item-info'>
-          <span className="title">모동숲 새해 카운트 다운</span>
-            <span className="viewer-count">조회수 54만회</span>
-          </div>
-        </figure>
-
-        <figure className='shorts-item'>
-          <img className='shorts-item-img' src='https://i.ytimg.com/vi/ThJBisdZB1Y/oar2.jpg?sqp=-oaymwEoCJUDENAFSFqQAgHyq4qpAxcIARUAAIhC2AEB4gEKCBgQAhgGOAFAAQ==&rs=AOn4CLDhYHosGr9qX6ZKrtIqDQriUiFHBA' alt='미리보기 이미지' />
-          <div className='shorts-item-info'>
-          <span className="title">모동숲 새해 카운트 다운</span>
-            <span className="viewer-count">조회수 54만회</span>
-          </div>
-        </figure>
-
+        {shorts.map((short, index) => (
+          <figure className="shorts-item" key={index}>
+            <img
+              className='shorts-item-img'
+              src={short.thumbUrl}
+              alt='미리보기 이미지'
+            />
+            <div className='shorts-item-info'>
+              <span className='title'>{short.title}</span>
+              <span className='viewer-count'>조회수 {formatViewerCount(short.viewerCount)}</span>
+            </div>
+          </figure>
+        ))}
       </div>
     </div>
   );
