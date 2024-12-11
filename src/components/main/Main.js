bb5d9f2c5aa3df81fe42f4823f0c4db4fedc7b20
happygirlll <lbm000314@gmail.com>
2024-12-12 00:42:24 +0900

    💄[main] MainVideos 연결

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CategoryBar from "./category/CategoryBar";
-
+import MainVideos from "./videos/MainVideos"
 const Main = () => {
     return (
         <div>
             <CategoryBar/>
+            <MainVideos/>
         </div>
     );
 };
---
+++ src/components/main/videos/MainVideos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const All = () => {
+    return (
+        <div>
+            <h2>전체 카테고리</h2>
+        </div>
+    );
+};
+
+export default All;
