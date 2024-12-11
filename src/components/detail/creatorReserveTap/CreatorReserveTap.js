0aa17d895681732e178d55497ff7394f47e3138d
saaad99 <ehdgnstla@naver.com>
2024-12-11 19:07:42 +0900

    🗑️ formatViewerCount 메소드를 utils 폴더로 뺌에 따라 기존 메소드 삭제

--- src/components/detail/creatorReserveTap/CreatorReserveTap.js
+++ src/components/detail/creatorReserveTap/CreatorReserveTap.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import formatViewerCount from "../../../utils/formatViewerCount.js";
 import "./CreatorReserveTap.css";
 
 function CreatorReserveTap() {
@@ -37,15 +38,6 @@ function CreatorReserveTap() {
       timestamp: "1:13:41",
     },
   ]);
-  
-  // 숫자로 들어온 조회수 데이터를 1만 단위로 표시 함수
-  function formatViewerCount(count) {
-    if (count < 10000) {
-      return `${count}회`; // 1만 미만은 그대로 표시
-    }
-    const formattedCount = (count / 10000).toFixed(); // 1만 단위로 나누고 소수점 1자리까지 표시
-    return `${formattedCount}만회`;
-  }
   
   // 스크롤 이벤트
   const handleScroll = (direction) => {
