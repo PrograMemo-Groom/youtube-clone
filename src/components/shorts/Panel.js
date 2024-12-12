a9423e00f9e96071a3a2fd2f6c5f028d8c22472c
S2ej1n <ks320205@naver.com>
2024-12-12 14:54:14 +0900

    💄 클래스 이름 규칙에 맞게  변경

--- src/components/shorts/Panel.js
+++ src/components/shorts/Panel.js
@@ -4,15 +4,15 @@ import styles from './Panel.module.css';
 const Panel = () => {
   return (
     <div className={styles.panelContainer}>
-        <div className={styles.contents_container}>
-          <header className={styles.contents_header}>
+        <div className={styles.contentsContainer}>
+          <header className={styles.contentsHeader}>
             <p>설명</p>
-            <button>X</button>
+            <button className={styles.closeBnt}>X</button>
           </header>
-          <section className={styles.contents_section}>
+          <section className={styles.contentsSection}>
             <p>한국과 전혀 다른 트와이스 미국 인기 반응</p>
           </section>
-          <section className={styles.contents_factoid}>
+          <section className={styles.contentsFactoid}>
             ddd
           </section>
         </div>
