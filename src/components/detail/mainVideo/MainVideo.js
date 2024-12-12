e856f2205dda35b884fbcaa23936bc8c8cb565ef
saaad99 <ehdgnstla@naver.com>
2024-12-12 17:22:16 +0900

    🐛 콘솔 오류 잡음

--- src/components/detail/mainVideo/MainVideo.js
+++ src/components/detail/mainVideo/MainVideo.js
@@ -94,7 +94,7 @@ function MainVideo() {
           <span>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              enable-background='new 0 0 24 24'
+              enableBackground='new 0 0 24 24'
               height='24'
               viewBox='0 0 24 24'
               width='24'
@@ -114,7 +114,7 @@ function MainVideo() {
         <div className='comment-list'>
           {/* 댓글 리스트 반환 */}
           {comments.map((comment) => (
-            <div className='comment'>
+            <div className='comment' key={comment.id}> 
             <img src={comment.userImg} alt='사용자 이미지' />
 
             <div className='comment-contents'>
