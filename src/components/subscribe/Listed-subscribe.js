2b64144478b3250f9a61439c488ce6dff94257a8
yoorym-kim <twilight99me@gmail.com>
2024-12-12 23:00:41 +0900

    ✨버튼 클릭시 뷰 전환(격자형, 리스트, 매니지 페이지로)

--- src/components/subscribe/Listed-subscribe.js
+++ src/components/subscribe/Listed-subscribe.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Listed-subscribe.module.css';
+import GridSubscribe from './Grid-subscribe';
+import ManageSubscribe from './Manage-subscribe';
 
 const ListedSubscribe = () => {
+
+    const [view, setView] = useState("list");
+
     return (
         <div className={styles.container}>
-            Listed-subscribe
-        </div>
+
+            {/* manage로 뷰 바뀌는 부분(헤더까지 바뀜 */}
+            {view === "manage" && <ManageSubscribe />}
+            {view === "grid" && <GridSubscribe />}
+            {view === "list" && (
+                <>
+                    <main>
+
+                    </main>
+                    <div>
+                        <h3>최신순</h3>
+                        <div className={styles.pageChangeButtons}>
+                            <button
+                                className={styles.manageButton}
+                                onClick={() => setView("manage")}
+                            >
+                                관리
+                            </button>
+                            <button
+                                className={styles.gridButton}
+                                onClick={() => setView("grid")}
+                            >
+                                <img alt='격자형'/>
+                            </button>
+                            <button
+                                className={styles.listButton}
+                                onClick={() => setView("list")}
+                            >
+                                <img alt='리스트형'/>
+                            </button>
+                        </div>
+                    </div>
+                </>
+            )}
+        </div>        
     );
 };
 
