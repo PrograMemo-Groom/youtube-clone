950d82fc2e5076cbfc4a71e6c1cef55fb047630b
happygirlll <lbm000314@gmail.com>
2024-12-12 18:09:19 +0900

    🎨[sidebar] 사이드바 버튼 클릭시 아이콘 색상 변경되게 코드 개선

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,35 +5,63 @@ import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
     const navigate = useNavigate();
 
+    const [selected, setSelected] = React.useState({
+            home: false,
+            shorts: false,
+            subscribe: false,
+            myPage: false,
+        }
+    );
+
+    const handleGoto = (url, item) => {
+        navigate(url);
+        setSelected(prev => ({
+            home: false,
+            shorts: false,
+            subscribe: false,
+            myPage: false,
+            [item]: true
+        }));
+    }
     return (
         <div className={styles.container}>
             <div
                 className={styles.sidebar_Item}
-                onClick={() => navigate('/')}
+                onClick={() => handleGoto('/','home')}
             >
-                <img src={`${process.env.PUBLIC_URL}/home.png`} alt="홈" className={styles.sidebar_icon}/>
+                {selected.home
+                    ? <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/home_select.svg`} alt="홈" className={styles.sidebar_icon}/>
+                    : <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/home.svg`} alt="홈" className={styles.sidebar_icon}/>
+                }
                 <span>홈</span>
             </div>
             <div
                 className={styles.sidebar_Item}
-                onClick={() => navigate('/shorts')}
+                onClick={() => handleGoto('/shorts', 'shorts')}
             >
-                <img src={`${process.env.PUBLIC_URL}/shorts.png`} alt="Shorts" className={styles.sidebar_icon}/>
+                {selected.shorts
+                    ? <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/shorts_select.svg`} alt="Shorts" className={styles.sidebar_icon} />
+                    : <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/shorts.svg`} alt="Shorts" className={styles.sidebar_icon} />
+                }
                 <span>Shorts</span>
             </div>
             <div
                 className={styles.sidebar_Item}
-                onClick={() => navigate('/subscribe')}
+                onClick={() => handleGoto('/subscribe', 'subscribe')}
             >
-                <img src={`${process.env.PUBLIC_URL}/subscription.png`} alt="구독" className={styles.sidebar_icon}/>
-                <span>구독</span>
+                {selected.subscribe
+                    ? <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/subscribe_select.svg`} alt="구독" className={styles.sidebar_icon} />
+                    : <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/subscribe.svg`} alt="구독" className={styles.sidebar_icon} />
+                }<span>구독</span>
             </div>
             <div
                 className={styles.sidebar_Item}
-                onClick={() => navigate('/myPage')}
+                onClick={() => handleGoto('/myPage', 'myPage')}
             >
-                <img src={`${process.env.PUBLIC_URL}/mypage.png`} alt="내 페이지" className={styles.sidebar_icon}/>
-                <span>내 페이지</span>
+                {selected.myPage
+                    ? <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/mypage_select.svg`} alt="내 페이지" className={styles.sidebar_icon} />
+                    : <img src={`${process.env.PUBLIC_URL}/assets/white/sidebar/mypage.svg`} alt="내 페이지" className={styles.sidebar_icon} />
+                }<span>내 페이지</span>
             </div>
         </div>
     );
