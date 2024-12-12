38c36695653f423fbd19a9c08582298123040ec0
jdaily00 <kyeonghee_@naver.com>
2024-12-12 20:32:44 +0900

    ✨ 마이페이지: 레이아웃 및 비율별 반응형 웹 (축소 완료, 확대 진행중)

--- src/components/myPage/MyPage.js
+++ src/components/myPage/MyPage.js
@@ -52,97 +52,134 @@ const videoData = [{
 
 export default function MyPage() {
     return (
-        <div className="contents-container">
-            <div className="user-page-info-container">
-                <img className="user-profile-img"
-                     src="/assets/mypage/user-profile.png"
-                     alt="user-profile-img"/>
-                <div className="user-name-and-id-container">
-                    <section className="user-name-container">
-                        <p className="user-name">공공</p>
-                        <a href="https://www.naver.com/"
-                           className="user-channel-move">
-                            @o0_o0_o0 &#183; 채널 보기</a>
-                    </section>
-                    <div className="changes-container">
-                        <section className="changes-id-container">
-                            <img className="chang-id-icon"
-                                 src="/assets/mypage/profile-icon.svg"
-                                 alt="user-pforile-icon"/>
-                            <p className="changes-id-text">계정 전환</p>
-                        </section>
-                        <div className="Google-id-change-container">
-                            <section className="Google-id-container">
-                                <img className="chang-id-icon"
-                                     src="/assets/mypage/google-logo-icon.svg"
-                                     alt="google-id"/>
-                                <p className="changes-id-text">Google 계정</p>
+        <div className="container">
+            <div className="relative-layout-container">
+                <div className="absolute-layout-container">
+                    <div className="contents-container">
+                        <div className="user-page-info-container">
+                            <img className="user-profile-img"
+                                 src="/assets/mypage/user-profile.png"
+                                 alt="user-profile-img"/>
+                            <div className="user-name-and-id-container">
+                                <section className="user-name-container">
+                                    <p className="user-name">공공</p>
+                                    <a href="https://www.naver.com/"
+                                       className="user-channel-move">
+                                        @o0_o0_o0 &#183; 채널 보기</a>
+                                </section>
+                                <div className="changes-container">
+                                    <section className="changes-id-container">
+                                        <img className="chang-id-icon"
+                                             src="/assets/mypage/profile-icon.svg"
+                                             alt="user-pforile-icon"/>
+                                        <p className="changes-id-text">계정 전환</p>
+                                    </section>
+                                    <div className="Google-id-change-container">
+                                        <section className="Google-id-container">
+                                            <img className="chang-id-icon"
+                                                 src="/assets/mypage/google-logo-icon.svg"
+                                                 alt="google-id"/>
+                                            <p className="changes-id-text">Google 계정</p>
+                                        </section>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="view-record-container">
+                            <section className="view-record-text-btn">
+                                <p>기록</p>
+                                <button className="all-video-view">모두 보기</button>
+                            </section>
+                            <section className="view-record-contents-container">
+                                <section className="video-list">
+                                    {videoData.map((video, i) => (
+                                        <section className="video-item"
+                                                 key={`${i}-${video.videoId}`}>
+                                            <div className="video-thumbnail-container">
+                                                <img className="video-thumbnail"
+                                                     src={video.thumbnail}
+                                                     alt={video.title}/>
+                                            </div>
+                                            <div className="video-info-container">
+                                                <h3 className="video-title">{video.title}</h3>
+                                                <p className="video-channel">{video.channel}</p>
+                                                <p className="video-meta">
+                                                    {video.view} · {video.uploadedAt}
+                                                </p>
+                                            </div>
+                                        </section>
+                                    ))}
+                                    <button className="next-video-btn"> ></button>
+                                </section>
+                            </section>
+                        </div>
+                        <div className="playlist-container">
+                            <section className="playlist-text-btn">
+                                <section className="playlist-sort-text">
+                                    <p className="playlist-text">재생목록</p>
+                                    <button className="sort-text">가나다순 &#9660;</button>
+                                </section>
+                                <section className="playlist-all-and-plus-btn">
+                                    <button className="plus-btn">+</button>
+                                    <button className="playlist-all-view">모두 보기</button>
+                                </section>
+                            </section>
+                            <section className="playlist-contents-container">
+                                <section className="playlist-list">
+                                    {videoData.map((video, i) => (
+                                        <section className="playlist-video-item"
+                                                 key={`${i}-${video.videoId}`}>
+                                            <div className="playlist-video-thumbnail-container">
+                                                <img className="playlist-video-thumbnail"
+                                                     src={video.thumbnail}
+                                                     alt={video.title}/>
+                                            </div>
+                                            <div className="playlist-video-info-container">
+                                                <h3 className="playlist-video-title">{video.title}</h3>
+                                                <p className="playlist-video-channel">{video.channel} &#183; 재생목록</p>
+                                                <p className="playlist-video-meta">
+                                                    모든 재생목록 보기
+                                                </p>
+                                            </div>
+                                        </section>
+                                    ))}
+                                    <button className="playlist-next-video-btn"> ></button>
+                                </section>
+                            </section>
+                        </div>
+                        <div className="Videos-to-watch-container">
+                            <section className="Videos-to-watch-text-btn">
+                                <section className="Videos-to-watch-later">
+                                    <p>나중에 볼 동영상</p>
+                                    <p className="watch-later-num">6</p>
+                                </section>
+                                <button className="all-video-view">모두 보기</button>
+                            </section>
+                            <section className="view-record-contents-container">
+                                <section className="video-list">
+                                    {videoData.map((video, i) => (
+                                        <section className="video-item"
+                                                 key={`${i}-${video.videoId}`}>
+                                            <div className="video-thumbnail-container">
+                                                <img className="video-thumbnail"
+                                                     src={video.thumbnail}
+                                                     alt={video.title}/>
+                                            </div>
+                                            <div className="video-info-container">
+                                                <h3 className="video-title">{video.title}</h3>
+                                                <p className="video-channel">{video.channel}</p>
+                                                <p className="video-meta">
+                                                    {video.view} · {video.uploadedAt}
+                                                </p>
+                                            </div>
+                                        </section>
+                                    ))}
+                                    <button className="next-video-btn"> ></button>
+                                </section>
                             </section>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="view-record-container">
-                <section className="view-record-text-btn">
-                    <p>기록</p>
-                    <button className="all-video-view">모두 보기</button>
-                </section>
-                <section className="view-record-contents-container">
-                    <section className="video-list">
-                        {videoData.map((video, i) => (
-                            <section className="video-item"
-                                     key={`${i}-${video.videoId}`}>
-                                <div className="video-thumbnail-container">
-                                    <img className="video-thumbnail"
-                                         src={video.thumbnail}
-                                         alt={video.title}/>
-                                </div>
-                                <div className="video-info-container">
-                                    <h3 className="video-title">{video.title}</h3>
-                                    <p className="video-channel">{video.channel}</p>
-                                    <p className="video-meta">
-                                        {video.view} · {video.uploadedAt}
-                                    </p>
-                                </div>
-                            </section>
-                        ))}
-                        <button className="next-video-btn"> ></button>
-                    </section>
-                </section>
-            </div>
-            <div className="playlist-container">
-                <section className="playlist-text-btn">
-                    <section className="playlist-sort-text">
-                        <p className="playlist-text">재생목록</p>
-                        <button className="sort-text">가나다순 &#9660;</button>
-                    </section>
-                    <section className="playlist-all-and-plus-btn">
-                        <button className="plus-btn">+</button>
-                        <button className="playlist-all-view">모두 보기</button>
-                    </section>
-                </section>
-                <section className="playlist-contents-container">
-                    <section className="playlist-list">
-                        {videoData.map((video, i) => (
-                            <section className="playlist-video-item"
-                                     key={`${i}-${video.videoId}`}>
-                                <div className="playlist-video-thumbnail-container">
-                                    <img className="playlist-video-thumbnail"
-                                         src={video.thumbnail}
-                                         alt={video.title}/>
-                                </div>
-                                <div className="playlist-video-info-container">
-                                    <h3 className="playlist-video-title">{video.title}</h3>
-                                    <p className="playlist-video-channel">{video.channel} &#183; 재생목록</p>
-                                    <p className="playlist-video-meta">
-                                        모든 재생목록 보기
-                                    </p>
-                                </div>
-                            </section>
-                        ))}
-                        <button className="playlist-next-video-btn"> ></button>
-                    </section>
-                </section>
             </div>
         </div>
     )
