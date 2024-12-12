7c855d36eb69a88da7cc00d75aa1bd7fdcb90aab
S2ej1n <ks320205@naver.com>
2024-12-12 11:16:45 +0900

    🔧 파일명 변경

--- src/components/shorts/shorts.js
+++ src/components/shorts/shorts.js
@@ -3,6 +3,7 @@ import styles from './Shorts.module.css';
 import Video from './Video.js';
 import Panel from './Panel.js';
 
+// 파일명 바꿨는덴
 function Shorts() {
   return (
     <main className={styles.Shorts_Container}>
