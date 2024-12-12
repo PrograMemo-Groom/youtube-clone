4ecae7c4654b5ed76461f16109c9c9cbd73cbf63
jdaily00 <kyeonghee_@naver.com>
2024-12-12 10:50:05 +0900

    ✨ 내 페이지: 재생목록 콘텐츠 추가 (더미데이터)

--- src/components/myPage/MyPage.js
+++ src/components/myPage/MyPage.js
@@ -106,41 +106,46 @@ export default function MyPage() {
                                 </div>
                             </section>
                         ))}
-                        <button className="next-video-btn"> > </button>
+                        <button className="next-video-btn"> ></button>
                     </section>
                 </section>
             </div>
 
             {/* 재생목록 */}
-            {/*<div className="playlist-container">*/}
-            {/*    <section className="playlist-text-btn">*/}
-            {/*        <p>재생목록</p>*/}
-            {/*        <button>가나다순&#9660;</button>*/}
-            {/*        <button>+</button>*/}
-            {/*        <button>모두 보기</button>*/}
-            {/*    </section>*/}
-            {/*    <section className="playlist-contents-container">*/}
-            {/*        <section className="playlist-list">*/}
-            {/*            {videoData.map((video, i) => (*/}
-            {/*                <section className="playlist-video-item"*/}
-            {/*                         key={`${i}-${video.videoId}`}>*/}
-            {/*                    <div className="playlist-video-thumbnail-container">*/}
-            {/*                        <img className="playlist-video-thumbnail"*/}
-            {/*                             src={video.thumbnail}*/}
-            {/*                             alt={video.title}/>*/}
-            {/*                    </div>*/}
-            {/*                    <div className="playlist-video-info-container">*/}
-            {/*                        <h3 className="playlist-video-title">{video.title}</h3>*/}
-            {/*                        <p className="playlist-video-channel">{video.channel}</p>*/}
-            {/*                        <p className="playlist-video-meta">*/}
-            {/*                            {video.view} · {video.uploadedAt}*/}
-            {/*                        </p>*/}
-            {/*                    </div>*/}
-            {/*                </section>*/}
-            {/*            ))}*/}
-            {/*        </section>*/}
-            {/*    </section>*/}
-            {/*</div>*/}
+            <div className="playlist-container">
+                <section className="playlist-text-btn">
+                    <section className="playlist-sort-text">
+                        <p className="playlist-text">재생목록</p>
+                        <button className="sort-text">가나다순 &#9660;</button>
+                    </section>
+                    <section className="playlist-all-and-plus-btn">
+                        <button className="plus-btn">+</button>
+                        <button className="playlist-all-view">모두 보기</button>
+                    </section>
+                </section>
+                <section className="playlist-contents-container">
+                    <section className="playlist-list">
+                        {videoData.map((video, i) => (
+                            <section className="playlist-video-item"
+                                     key={`${i}-${video.videoId}`}>
+                                <div className="playlist-video-thumbnail-container">
+                                    <img className="playlist-video-thumbnail"
+                                         src={video.thumbnail}
+                                         alt={video.title}/>
+                                </div>
+                                <div className="playlist-video-info-container">
+                                    <h3 className="playlist-video-title">{video.title}</h3>
+                                    <p className="playlist-video-channel">{video.channel} &#183; 재생목록</p>
+                                    <p className="playlist-video-meta">
+                                        모든 재생목록 보기
+                                    </p>
+                                </div>
+                            </section>
+                        ))}
+                        <button className="playlist-next-video-btn"> ></button>
+                    </section>
+                </section>
+            </div>
 
         </div>
     )
