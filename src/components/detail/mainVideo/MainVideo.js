06e15b376f6b49b9d9fc84e06b4c71a9216c50ad
saaad99 <ehdgnstla@naver.com>
2024-12-12 11:19:33 +0900

    💄 Detail 컴포넌트 스택형으로 쌓일수 있게 relative하게 기존의 absolute값들 일부 변경

--- src/components/detail/mainVideo/MainVideo.js
+++ src/components/detail/mainVideo/MainVideo.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./MainVideo.css"
+import React from "react";
+import "./MainVideo.css";
 
 function MainVideo() {
   return (
     <div className='mainVideo-container'>
-      
+      <div className="video-container">
+        <iframe
+          width='560'
+          height='315'
+          src='https://www.youtube.com/embed/rZ3tsvTqeZ0?si=-SH64CToqfeVyIfP'
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          referrerpolicy='strict-origin-when-cross-origin'
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MainVideo
+export default MainVideo;
