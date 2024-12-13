4f8c7ba45a492508fcacbd61b021d6f414492487
Jae-Hong-Kim <newfly101@naver.com>
2024-12-13 15:26:17 +0900

    🔥️ API dummy Data 삭제 및 API 요청 최적화

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,83 +1,8 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import styles from "./Search.module.css";
 import {useLocation} from "react-router-dom";
-import {fetchSearchVideos} from "../../service/VideoService";
+import {fetchSearchList} from "../../service/SearchService";
 import useNavigation from "../../hooks/useNavigation";
-
-// const videoData = {
-//     "pageToken": "CAMQAA",
-//     "items": [
-//         {
-//             "videoId": "mk5DtMGYLGU",
-//             "channelId": "UCF4Wxdo3inmxP-Y59wXDsFw",
-//             "channelTitle": "MBCNEWS",
-//             "description": "12·3 내란 사태와 탄핵 정국은 온라인 이슈를 한순간에 뒤덮었습니다. 구글 트렌드 검색 데이터에 따르면, 지난 2일부터 9일까지 국내 ...",
-//             "publishTime": "2024-12-10T22:13:40Z",
-//             "defaultThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/mk5DtMGYLGU/default.jpg",
-//                 "width": 120,
-//                 "height": 90
-//             },
-//             "highThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/mk5DtMGYLGU/hqdefault.jpg",
-//                 "width": 480,
-//                 "height": 360
-//             },
-//             "mediumThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/mk5DtMGYLGU/mqdefault.jpg",
-//                 "width": 320,
-//                 "height": 180
-//             },
-//             "title": "[와글와글] 구글서 &#39;계엄령&#39;·&#39;윤석열&#39; 검색량 폭증 (2024.12.11/뉴스투데이/MBC)"
-//         },
-//         {
-//             "videoId": "aMtvJoE_EJQ",
-//             "channelId": "UCIUni4ScRp4mqPXsxy62L5w",
-//             "channelTitle": "언더스탠딩 : 세상의 모든 지식",
-//             "description": "언더스탠딩 문의: to.understanding.official@gmail.com 글로 읽는 \"언더스탠딩 텍스트\".",
-//             "publishTime": "2024-09-05T09:15:00Z",
-//             "defaultThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/aMtvJoE_EJQ/default.jpg",
-//                 "width": 120,
-//                 "height": 90
-//             },
-//             "highThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/aMtvJoE_EJQ/hqdefault.jpg",
-//                 "width": 480,
-//                 "height": 360
-//             },
-//             "mediumThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/aMtvJoE_EJQ/mqdefault.jpg",
-//                 "width": 320,
-//                 "height": 180
-//             },
-//             "title": "(1부) 구글 검색의 시대 끝날 것 같습니다 (솔트룩스 이경일 대표)"
-//         },
-//         {
-//             "videoId": "Pw_Mzxtyarc",
-//             "channelId": "UCsU-I-vHLiaMfV_ceaYz5rQ",
-//             "channelTitle": "JTBC News",
-//             "description": "챗GPT를 개발한 오픈AI가 검색엔진 기능을 소개했습니다. 네이버나 구글 같은 역할을 하게 되는데, 인공지능은 머지않아 개인 비서로 ...",
-//             "publishTime": "2024-10-31T23:39:02Z",
-//             "defaultThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/Pw_Mzxtyarc/default.jpg",
-//                 "width": 120,
-//                 "height": 90
-//             },
-//             "highThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/Pw_Mzxtyarc/hqdefault.jpg",
-//                 "width": 480,
-//                 "height": 360
-//             },
-//             "mediumThumbnail": {
-//                 "url": "https://i.ytimg.com/vi/Pw_Mzxtyarc/mqdefault.jpg",
-//                 "width": 320,
-//                 "height": 180
-//             },
-//             "title": "&#39;구글에 정면 도전한다&#39;…오픈AI &#39;챗GPT 검색&#39; 기능 출시 #소셜픽 / JTBC 아침&amp;"
-//         }
-//     ]
-// }
 
 const tag = '[SearchPage]';
 const Search = () => {
@@ -93,7 +18,7 @@ const Search = () => {
     useEffect(() => {
         const fetchData = async () => {
             if (searchTerm.trim().length > 0) {
-                const response = await fetchSearchVideos(searchTerm);
+                const response = await fetchSearchList(searchTerm);
                 console.log(tag, "fetchData:", response);
                 setSearchResult(response.items || []);
                 setNextToken(response.pageToken || "");
@@ -123,9 +48,6 @@ const Search = () => {
             </div>
             <div className={styles.videos}>
                 {searchResult && searchResult.map((video) => {
-                    // console.log(video);
-                    // const thisData = new Date();
-                    // console.log(today)
                     return (
                         <div className={styles.videoLists} key={video.videoId}>
                             <div className={styles.videoFrame} onClick={() => handleShowVideo(video.videoId)}>
