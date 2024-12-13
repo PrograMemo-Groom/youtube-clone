fb86416e1d07d45368a7063fecd0040b4d749a30
happygirlll <lbm000314@gmail.com>
2024-12-13 11:08:26 +0900

    💄MainService.js에 있는 formatPublishedDate함수 지우고 utils 파일에 있는 formatTimeDifference함수 사용하기

--- src/service/MainService.js
+++ src/service/MainService.js
@@ -1,5 +1,6 @@
 import instance from "../api/api";
 import requests from "../api/endpoint";
+import formatTimeDifference from "../utils/formatTimeDifference";
 
 // 채널 프로필 이미지 요청 함수
 const getChannelThumbnail = async (channelId) => {
@@ -47,34 +48,6 @@ const formatViewCount = (viewCount) => {
     return `${viewCount}회`;
 };
 
-// 동영상 게시 일자 표현 : 현재 - 게시일
-const formatPublishedDate = (publishedDate) => {
-    const now = new Date();
-    const published = new Date(publishedDate);
-    const Seconds = Math.floor((now - published) / 1000);
-
-    const Minute = 60;
-    const Hour = 60 * Minute;
-    const Day = 24 * Hour;
-    const Month = 30 * Day;
-    const Year = 12 * Month;
-
-    if (Seconds < Day) {
-        const hours = Math.floor(Seconds / Hour);
-        return `${hours}시간 전`;
-    } else if (Seconds < Month) {
-        const days = Math.floor(Seconds / Day);
-        return `${days}일 전`;
-    } else if (Seconds < Year) {
-        const months = Math.floor(Seconds / Month);
-        return `${months}개월 전`;
-    } else {
-        const years = Math.floor(Seconds / Year);
-        return `${years}년 전`;
-    }
-};
-
-
 //메인 동영상 가져오기
 export const getMainVideos = async () => {
     try {
@@ -83,7 +56,7 @@ export const getMainVideos = async () => {
                 part: "snippet,contentDetails,statistics",
                 chart: "mostPopular",
                 regionCode: "KR",
-                maxResults: 32,
+                maxResults: 4,
             },
         });
         // 동영상 데이터 가공
@@ -98,7 +71,7 @@ export const getMainVideos = async () => {
                     thumbnail: item.snippet.thumbnails.high.url,
                     time: formatDuration(item.contentDetails.duration),
                     profile: channelThumbnail,
-                    stats: `조회수 ${formatViewCount(item.statistics.viewCount)} · ${formatPublishedDate(item.snippet.publishedAt)}`,
+                    stats: `조회수 ${formatViewCount(item.statistics.viewCount)} · ${formatTimeDifference(item.snippet.publishedAt)}`,
                 };
             })
         );
