f877c2a5afda319960dd8ae56509d789d2ead0f0
happygirlll <lbm000314@gmail.com>
2024-12-13 17:05:30 +0900

    💄[main] 더보기 메뉴에 아이콘 파일 추가 및 UI 조정

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -98,14 +98,70 @@ const MainVideos = ({ fetchFunction }) => {
                                     {openDropdown === video.videoId && (
                                         <div className={styles.dropdownMenu}>
                                             <ul>
-                                                <li>현재 재생목록에 추가</li>
-                                                <li>나중에 볼 동영상에 저장</li>
-                                                <li>재생목록에 저장</li>
-                                                <li>오프라인 저장</li>
-                                                <li>공유</li>
-                                                <li>관심 없음</li>
-                                                <li>채널 추천 안함</li>
-                                                <li>신고</li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/playlist.svg`}
+                                                        alt="현재 재생목록에 추가"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    현재 재생목록에 추가
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/clock.svg`}
+                                                        alt="나중에 볼 동영상에 저장"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    나중에 볼 동영상에 저장
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/bookmark.svg`}
+                                                        alt="재생목록에 저장"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    재생목록에 저장
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/download.svg`}
+                                                        alt="오프라인 저장"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    오프라인 저장
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/share.svg`}
+                                                        alt="공유"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    공유
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/wrong.svg`}
+                                                        alt="관심 없음"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    관심 없음
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/no.svg`}
+                                                        alt="채널 추천 안함"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    채널 추천 안함
+                                                </li>
+                                                <li>
+                                                    <img
+                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/flag.svg`}
+                                                        alt="신고"
+                                                        className={styles.menuIcon}
+                                                    />
+                                                    신고
+                                                </li>
                                             </ul>
                                         </div>
                                     )}
