188e319a7de33fc89463253ae4988d3b30784431
happygirlll <lbm000314@gmail.com>
2024-12-14 11:18:08 +0900

    💄[main] 커밋 문제 되는 코드 수정

--- src/components/main/category/CategoryBar.js
+++ src/components/main/category/CategoryBar.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import styles from "./CategoryBar.module.css";
 import {
     fetchRecentlyUploaded,
-    fetchWatchHistory,
+    //fetchWatchHistory,
     fetchPersonalizedVideos,
 } from "./CategoryAPI";
 
