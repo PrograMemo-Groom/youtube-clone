18ea9ebedb44af295ed5720710e40f8a2c7457e3
happygirlll <lbm000314@gmail.com>
2024-12-14 15:14:46 +0900

    ✨[main] 사용자 프로필 클릭하면 해당 프로필 페이지로 이동하게 구현

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MainVideos.module.css";
 import { getMainVideos } from "../../../service/MainService";
 import useNavigation from "../../../hooks/useNavigation";
 
 const MainVideos = ({ fetchFunction }) => {
-    const [videos, setVideos] = useState([]);// 비디오 데이터를 저장할 상태
-    const [loading, setLoading] = useState(true); //로딩 상태
-    const [error, setError] = useState(null); //에러 상태
+    const [videos, setVideos] = useState([]); // 비디오 데이터를 저장할 상태
+    const [loading, setLoading] = useState(true); // 로딩 상태
+    const [error, setError] = useState(null); // 에러 상태
     const [hoveredVideo, setHoveredVideo] = useState(null); // 현재 호버 중인 비디오 ID
-    const [openDropdown, setOpenDropdown] = useState(null); //더보기 메뉴
+    const [openDropdown, setOpenDropdown] = useState(null); // 더보기 메뉴
     const { link } = useNavigation();
 
     useEffect(() => {
@@ -20,10 +20,8 @@ const MainVideos = ({ fetchFunction }) => {
 
                 let videoData;
                 if (Array.isArray(fetchFunction)) {
-                    // 데이터 배열이 직접 전달된 경우
                     videoData = fetchFunction;
                 } else {
-                    // 숫자 ID일 경우 기존 로직 사용
                     videoData = await getMainVideos(fetchFunction);
                 }
 
@@ -40,12 +38,14 @@ const MainVideos = ({ fetchFunction }) => {
         fetchVideos();
     }, [fetchFunction]);
 
-
-
-
     const handleShowVideo = (videoId, event) => {
         if (event) event.stopPropagation(); // 이벤트 버블링 방지
         link(`/detail?q=${videoId}`);
+    };
+
+    const handleChannelClick = (channelId, event) => {
+        if (event) event.stopPropagation(); // 이벤트 버블링 방지
+        window.open(`https://www.youtube.com/channel/${channelId}`, "_blank");
     };
 
     const toggleDropdown = (videoId) => {
@@ -93,11 +93,10 @@ const MainVideos = ({ fetchFunction }) => {
                                 className={styles.profilePicture}
                                 alt="channel profile"
                                 src={video.profile}
+                                onClick={(event) => handleChannelClick(video.channelId, event)} // 유저 프로필 클릭 이벤트 추가
                             />
                         </div>
-                        <div
-                            className={styles.videoInfo}
-                        >
+                        <div className={styles.videoInfo}>
                             <div className={styles.titleRow}>
                                 <p
                                     className={styles.videoTitle}
@@ -105,7 +104,7 @@ const MainVideos = ({ fetchFunction }) => {
                                 >
                                     {video.title}
                                 </p>
-                                <div style={{position: "relative"}}>
+                                <div style={{ position: "relative" }}>
                                     <img
                                         src={`${process.env.PUBLIC_URL}/assets/icon/more_btn_black.svg`}
                                         alt="more"
@@ -115,88 +114,16 @@ const MainVideos = ({ fetchFunction }) => {
                                     {openDropdown === video.videoId && (
                                         <div className={styles.dropdownMenu}>
                                             <ul>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/playlist.svg`}
-                                                        alt="현재 재생목록에 추가"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    현재 재생목록에 추가
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/clock.svg`}
-                                                        alt="나중에 볼 동영상에 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    나중에 볼 동영상에 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/bookmark.svg`}
-                                                        alt="재생목록에 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    재생목록에 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/download.svg`}
-                                                        alt="오프라인 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    오프라인 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/share.svg`}
-                                                        alt="공유"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    공유
-                                                </li>
-                                                <hr className={styles.menuDivider}/>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/wrong.svg`}
-                                                        alt="관심 없음"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    관심 없음
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/no.svg`}
-                                                        alt="채널 추천 안함"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    채널 추천 안함
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/flag.svg`}
-                                                        alt="신고"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    신고
-                                                </li>
+                                                <li>현재 재생목록에 추가</li>
+                                                <li>나중에 볼 동영상에 저장</li>
+                                                <li>재생목록에 저장</li>
                                             </ul>
                                         </div>
                                     )}
                                 </div>
                             </div>
-                            <p
-                                className={styles.videoAuthor}
-                                onClick={(event) => handleShowVideo(video.videoId, event)}
-                            >
-                                {video.author}
-                            </p>
-                            <p
-                                className={styles.videoStats}
-                                onClick={(event) => handleShowVideo(video.videoId, event)}
-                            >
-                                {video.stats}
-                            </p>
+                            <p className={styles.videoAuthor}>{video.author}</p>
+                            <p className={styles.videoStats}>{video.stats}</p>
                         </div>
                     </div>
                 </div>
@@ -204,6 +131,5 @@ const MainVideos = ({ fetchFunction }) => {
         </div>
     );
 };
-
 
 export default MainVideos;
