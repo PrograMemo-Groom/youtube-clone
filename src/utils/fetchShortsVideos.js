bc96cdad25f28db89505dd1fe50c87d888c188fd
saaad99 <ehdgnstla@naver.com>
2024-12-14 14:51:01 +0900

    [파일추가] util폴더 shorts video 가져오는 fetch 함수 추가

--- src/utils/fetchShortsVideos.js
+++ src/utils/fetchShortsVideos.js
@@ -1,31 +1,23 @@
-import { API_KEY, API_URL } from "../config/config";
+import instance from "../../../api/api";
+import requests from "../../../api/endpoint";
 
-// Shorts 영상 리스트를 반환하는 함수
-export const fetchShortsVideos = async (keyword = "Shorts", maxResults = 10) => {
-  const url = `${API_URL}/search?part=snippet&maxResults=${maxResults}&q=${encodeURIComponent(
-    keyword
-  )}&type=video&videoDuration=short&key=${API_KEY}`;
-  
+export const fetchShortsVideos = async (query = "Faker") => {
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (data.items) {
-      return data.items.map((item) => ({
-        videoId: item.id.videoId,
-        title: item.snippet.title,
-        thumbnail: item.snippet.thumbnails.high.url,
-        channelTitle: item.snippet.channelTitle,
-        publishedAt: item.snippet.publishedAt,
-        videoLink: `https://www.youtube.com/watch?v=${item.id.videoId}`, // 영상 링크 추가
-      }));
-    } else {
-      return [];
-    }
-  } catch (error) {
-    console.error("YouTube Shorts 데이터를 가져오는 중 오류가 발생했습니다:", error);
-    throw new Error("YouTube Shorts 데이터를 가져오는 중 문제가 발생했습니다.");
+    const {
+      data: { items: response },
+    } = await instance.get(requests.fetchGetSearch, {
+      params: {
+        part: "snippet",
+        regionCode: "KR",
+        maxResults: 4,
+        q: query,
+        videoDuration: "short", // 짧은 영상 필터링
+        type: "video",
+      },
+    });
+    // console.log("fetchShortsVideos response",response);
+    return response;
+  } catch (e) {
+    console.log("fetchShortsVideo can't get response data", e);
   }
 };
-
-export default fetchShortsVideos;
