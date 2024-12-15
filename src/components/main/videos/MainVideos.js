f730cc482436b84b8d420611c71cbb48786c461c
happygirlll <lbm000314@gmail.com>
2024-12-15 21:18:23 +0900

    💄[main] 무한스크롤 구현 실패로 영상 개수 조정..

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MainVideos.module.css";
 import { getMainVideos } from "../../../service/MainService";
 import useNavigation from "../../../hooks/useNavigation";
