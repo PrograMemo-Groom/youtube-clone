d23070be3a72ae2ac2e1da37f4718a63f6a84d1f
yoorym-kim <twilight99me@gmail.com>
2024-12-15 18:11:33 +0900

    ✨매니지페이지/채널 리스트에 api데이터 보이게 설정, 더미데이터 삭제

--- src/components/subscribe/manage/Manage-subscribe.js
+++ src/components/subscribe/manage/Manage-subscribe.js
@@ -1,21 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Manage-subscribe.module.css';
-import {fetchSubscriptions} from "../../../service/SubscribeService";
+import useGoogleAuth from "../../../hooks/useGoogleAuth";
+import { fetchSubscriptions } from "../../../service/SubscribeService";
 
-
-const ManageSubscribe = ({ accessToken }) => {
+const ManageSubscribe = () => {
+    const [accessToken, setAccessToken] = useState(() => localStorage.getItem("GOOGLE_TOKEN"));
     const [subscriptions, setSubscriptions] = useState([]);
+    const googleLogin = useGoogleAuth();
 
-    useEffect(() => {
+
+        // 최초 인증 및 accessToken 만료시간 이후 재발급 받을 때 사용
+        const handleGetCode = async () => {
+            console.log(`handleLogin: 구글 로그인 다시 하는 중 ㅠㅠ`);
+            await googleLogin();
+        }
+    
+        useEffect(() => {
+            accessToken && fetchData();
+        }, [accessToken]);
+
         const fetchData = async () => {
-            const data = await fetchSubscriptions();
-            console.log('data',data);
-            setSubscriptions(data || []);
-        };
-        fetchData();
-    }, [accessToken]);
+            try {
+                if(!accessToken) {
+                    console.log("token없다이!!발급버튼 눌러서 발급받아라이!!");
+                    return;
+                }
+                const response = await fetchSubscriptions(accessToken);  // 구독 비디오오오
+                console.log("내가 구독하는 video 갖고 왔다이!!!!! ",response);
+                if (Array.isArray(response)) {
+                    console.log('내가 가져온 동영상들 배열성공 !!');
+                    const flattenedResponse = response.flatMap(sub => sub); //이중배열을 풀어보자
+                    const sortedResponse = flattenedResponse.sort((a, b) => {  // 영상들만 최신순 정렬하자
+                        return new Date(b.publishTime) - new Date(a.publishTime);
+                    });
+                    setSubscriptions(sortedResponse);
+                } else {
+                    console.error("받아온게 배열이 아님.. 이거임:", response);
+                }
+            } catch (error) {
+                console.log('fetchData 에러 :', error);
+            }
+        }
 
     return (
+        <>
+        <button
+            style={{width:'200px', height:'20px'}}
+            onClick={() => {handleGetCode();}}>
+                token 발급 받는다!!
+        </button>
+        <button
+            style={{width:'200px', height:'20px'}}
+            onClick={() => {fetchData()}}>
+                누르면 데이터를 가져오ㅏ
+        </button>
+        {accessToken &&
+            <button style={{width:'200px', height:'20px'}}>
+                token값 있으면 노출
+        </button>}
+
         <div className={styles.container}>
             <header className={styles.header}>
                 <h1>모든 구독 채널</h1>
@@ -31,17 +74,17 @@ const ManageSubscribe = ({ accessToken }) => {
                     subscriptions.map((channel , index) => (
                         <div key={channel.id} className={styles.channelCard}>
                             <img
-                                src={channel.profileImage}
-                                alt={`${channel.title} Profile`}
+                                src={channel.channelAvatar}
+                                alt='체널 아바타사진'
                                 className={styles.channelImage}
                             />
                             <div className={styles.channelDescription}>
-                                <h3>{channel.title}</h3>
+                                <h3>{channel.channelTitle}</h3>
                                 <p>@{channel.id}·구독자 {channel.subscriberCount}명</p>
-                                <p>{channel.description}</p>
+                                <p>{channel.channelDescription}</p>
                             </div>
                             {/* 구독알림설정 드롭다운박스 : api가져와서 변경 만이 필요,,*/}
-                            <div class={styles.buttonDiv}>
+                            <div className={styles.buttonDiv}>
                                 <select className={styles.alarmDropdown}> 
                                     <option value="alarmAll">전체</option>
                                     <option value="alarmSet">맞춤설정</option>
@@ -56,29 +99,8 @@ const ManageSubscribe = ({ accessToken }) => {
                 )} 
             </main>
         </div>
+        </>
     );
 };
 
 export default ManageSubscribe;
-
-
-const channelData = [{
-    title: "MochaMilk",
-    id: "MochaMilk",
-    subscriberCount: "167만",
-    profileImage: "https://yt3.googleusercontent.com/ytc/AIdro_mOjBZTu180Gt9tDIvv5TjwC_TJE0Gj39r0XrhkbdDaVR8=s176-c-k-c0x00ffffff-no-rj-mo",
-    description: "모카와 우유의 일상을 함께 봐주셔서 감사합니다 :) • 모카 생년월일: 2011.10.22 견종: 폼피츠 성별: 남 • 우유 생년월일: 2016.11.07 견종: 사모예드 성별: 여 _________________________________________________________ Thank you for watching MochaMilk's daily vlog :) • Mocha Birth: 2011.10.22",
-    } , {
-    title: "MochaMilk",
-    id: "MochaMilk",
-    subscriberCount: "167만",
-    profileImage: "https://yt3.googleusercontent.com/ytc/AIdro_mOjBZTu180Gt9tDIvv5TjwC_TJE0Gj39r0XrhkbdDaVR8=s176-c-k-c0x00ffffff-no-rj-mo",
-    description: "모카와 우유의 일상을 함께 봐주셔서 감사합니다 :) • 모카 생년월일: 2011.10.22 견종: 폼피츠 성별: 남 • 우유 생년월일: 2016.11.07 견종: 사모예드 성별: 여 _________________________________________________________ Thank you for watching MochaMilk's daily vlog :) • Mocha Birth: 2011.10.22",
-    } , {
-    title: "MochaMilk",
-    id: "MochaMilk",
-    subscriberCount: "167만",
-    profileImage: "https://yt3.googleusercontent.com/ytc/AIdro_mOjBZTu180Gt9tDIvv5TjwC_TJE0Gj39r0XrhkbdDaVR8=s176-c-k-c0x00ffffff-no-rj-mo",
-    description: "모카와 우유의 일상을 함께 봐주셔서 감사합니다 :) • 모카 생년월일: 2011.10.22 견종: 폼피츠 성별: 남 • 우유 생년월일: 2016.11.07 견종: 사모예드 성별: 여 _________________________________________________________ Thank you for watching MochaMilk's daily vlog :) • Mocha Birth: 2011.10.22",
-    }
-]
