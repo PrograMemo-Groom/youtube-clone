4829d694d0b2c31fe00dff627812aab1bf37b8d7
happygirlll <lbm000314@gmail.com>
2024-12-15 21:16:10 +0900

    🐛[main] 커밋에 문제 되는 코드 주석 처리

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -10,10 +10,10 @@ const MainVideos = ({ fetchFunction }) => {
     const [hoveredVideo, setHoveredVideo] = useState(null); // 현재 호버 중인 비디오 ID
     const [openDropdown, setOpenDropdown] = useState(null); // 더보기 메뉴
     const { link } = useNavigation();
-    const [products, setProducts] = useState([]);
-    const [hasMore, setHasMore] = useState(true);
-    const [page, setPage] = useState(0);
-    const elementRef = useRef(null);
+    //const [products, setProducts] = useState([]);
+    //const [hasMore, setHasMore] = useState(true);
+   // const [page, setPage] = useState(0);
+    //const elementRef = useRef(null);
 
     const normalizeThumbnails = (videos) => {
         return videos.map((video) => {
