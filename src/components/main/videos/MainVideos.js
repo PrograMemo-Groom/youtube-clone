e0b49c95a19b9f91d1881eef7b3ee1a7c3fa64d3
happygirlll <lbm000314@gmail.com>
2024-12-17 03:45:55 +0900

    💄[main] 코드 들여쓰기

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -107,149 +107,149 @@ const MainVideos = ({ fetchFunction }) => {
 
     return (
         <>
-        <div className={styles.videoGrid}>
-            {videos.map((video, index) => (
-                <div
-                    key={index}
-                    className={styles.videoPreview}
-                    onMouseEnter={() => setHoveredVideo(video.videoId)}
-                    onMouseLeave={() => setHoveredVideo(null)}
-                >
-                    <div className={styles.thumbnailRow}>
-                        {hoveredVideo === video.videoId ? (
-                            <iframe
-                                className={styles.videoPlayer}
-                                src={`https://www.youtube.com/embed/${video.videoId}?autoplay=1&mute=1`}
-                                title={video.title}
-                                allow="autoplay; encrypted-media"
-                                allowFullScreen
-                            ></iframe>
-                        ) : (
-                            <img
-                                className={styles.thumbnail}
-                                alt={video.title}
-                                src={video.thumbnail}
-                            />
-                        )}
-                        <div className={styles.videoTime}>{video.time}</div>
-                    </div>
-                    <div className={styles.videoInfoGrid}>
-                        <div className={styles.channelPicture}>
-                            <img
-                                className={styles.profilePicture}
-                                alt="channel profile"
-                                src={video.profile}
-                                onClick={(event) => handleChannelClick(video.channelId, event)} // 유저 프로필 클릭 이벤트 추가
-                            />
+            <div className={styles.videoGrid}>
+                {videos.map((video, index) => (
+                    <div
+                        key={index}
+                        className={styles.videoPreview}
+                        onMouseEnter={() => setHoveredVideo(video.videoId)}
+                        onMouseLeave={() => setHoveredVideo(null)}
+                    >
+                        <div className={styles.thumbnailRow}>
+                            {hoveredVideo === video.videoId ? (
+                                <iframe
+                                    className={styles.videoPlayer}
+                                    src={`https://www.youtube.com/embed/${video.videoId}?autoplay=1&mute=1`}
+                                    title={video.title}
+                                    allow="autoplay; encrypted-media"
+                                    allowFullScreen
+                                ></iframe>
+                            ) : (
+                                <img
+                                    className={styles.thumbnail}
+                                    alt={video.title}
+                                    src={video.thumbnail}
+                                />
+                            )}
+                            <div className={styles.videoTime}>{video.time}</div>
                         </div>
-                        <div className={styles.videoInfo}>
-                            <div className={styles.titleRow}>
+                        <div className={styles.videoInfoGrid}>
+                            <div className={styles.channelPicture}>
+                                <img
+                                    className={styles.profilePicture}
+                                    alt="channel profile"
+                                    src={video.profile}
+                                    onClick={(event) => handleChannelClick(video.channelId, event)} // 유저 프로필 클릭 이벤트 추가
+                                />
+                            </div>
+                            <div className={styles.videoInfo}>
+                                <div className={styles.titleRow}>
+                                    <p
+                                        className={styles.videoTitle}
+                                        onClick={(event) => handleShowVideo(video.videoId, event)}
+                                    >
+                                        {video.title}
+                                    </p>
+                                    <div style={{position: "relative"}}>
+                                        <img
+                                            src={`${process.env.PUBLIC_URL}/assets/icon/more_btn_black.svg`}
+                                            alt="more"
+                                            className={styles.more}
+                                            onClick={() => toggleDropdown(video.videoId)}
+                                        />
+                                        {openDropdown === video.videoId && (
+                                            <div className={styles.dropdownMenu}>
+                                                <ul>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/playlist.svg`}
+                                                            alt="현재 재생목록에 추가"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        현재 재생목록에 추가
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/clock.svg`}
+                                                            alt="나중에 볼 동영상에 저장"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        나중에 볼 동영상에 저장
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/bookmark.svg`}
+                                                            alt="재생목록에 저장"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        재생목록에 저장
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/download.svg`}
+                                                            alt="오프라인 저장"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        오프라인 저장
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/share.svg`}
+                                                            alt="공유"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        공유
+                                                    </li>
+                                                    <hr className={styles.menuDivider}/>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/wrong.svg`}
+                                                            alt="관심 없음"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        관심 없음
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/no.svg`}
+                                                            alt="채널 추천 안함"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        채널 추천 안함
+                                                    </li>
+                                                    <li>
+                                                        <img
+                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/flag.svg`}
+                                                            alt="신고"
+                                                            className={styles.menuIcon}
+                                                        />
+                                                        신고
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
                                 <p
-                                    className={styles.videoTitle}
+                                    className={styles.videoAuthor}
                                     onClick={(event) => handleShowVideo(video.videoId, event)}
                                 >
-                                    {video.title}
+                                    {video.author}
                                 </p>
-                                <div style={{position: "relative"}}>
-                                    <img
-                                        src={`${process.env.PUBLIC_URL}/assets/icon/more_btn_black.svg`}
-                                        alt="more"
-                                        className={styles.more}
-                                        onClick={() => toggleDropdown(video.videoId)}
-                                    />
-                                    {openDropdown === video.videoId && (
-                                        <div className={styles.dropdownMenu}>
-                                            <ul>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/playlist.svg`}
-                                                        alt="현재 재생목록에 추가"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    현재 재생목록에 추가
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/clock.svg`}
-                                                        alt="나중에 볼 동영상에 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    나중에 볼 동영상에 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/bookmark.svg`}
-                                                        alt="재생목록에 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    재생목록에 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/download.svg`}
-                                                        alt="오프라인 저장"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    오프라인 저장
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/share.svg`}
-                                                        alt="공유"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    공유
-                                                </li>
-                                                <hr className={styles.menuDivider}/>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/wrong.svg`}
-                                                        alt="관심 없음"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    관심 없음
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/no.svg`}
-                                                        alt="채널 추천 안함"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    채널 추천 안함
-                                                </li>
-                                                <li>
-                                                    <img
-                                                        src={`${process.env.PUBLIC_URL}/assets/videoMore/flag.svg`}
-                                                        alt="신고"
-                                                        className={styles.menuIcon}
-                                                    />
-                                                    신고
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    )}
-                                </div>
+                                <p
+                                    className={styles.videoStats}
+                                    onClick={(event) => handleShowVideo(video.videoId, event)}
+                                >
+                                    {video.stats}
+                                </p>
                             </div>
-                            <p
-                                className={styles.videoAuthor}
-                                onClick={(event) => handleShowVideo(video.videoId, event)}
-                            >
-                                {video.author}
-                            </p>
-                            <p
-                                className={styles.videoStats}
-                                onClick={(event) => handleShowVideo(video.videoId, event)}
-                            >
-                                {video.stats}
-                            </p>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    {loading && <div className={styles.loading}>로딩 중...</div>}
-    <div ref={observerRef} style={{ height: "1px", background: "transparent"  }}></div>
-</>
+                ))}
+            </div>
+            {loading && <div className={styles.loading}>로딩 중...</div>}
+            <div ref={observerRef} style={{ height: "1px", background: "transparent"  }}></div>
+        </>
     );
 };
 
