7b895ca7808c0d02469222c65f7f29c1ff076a00
Jae-Hong-Kim <newfly101@naver.com>
2024-12-18 15:52:12 +0900

    ♻️ Reducer: 기본 상태값에 맞게 변경

--- src/store/reducer/SearchReducer.js
+++ src/store/reducer/SearchReducer.js
@@ -11,14 +11,10 @@ const SearchReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_SEARCH':
             return {...state, searchResult: action.payload};
-        case 'SET_SEARCH_ERROR':
-            return {...state, error: action.payload};
         case 'SET_NEXT_TOKEN':
             return {...state, nextToken: action.payload};
-        case 'SET_NEXT_TOKEN_ERROR':
-            return {...state, error: null};
         case 'SET_MOUSE_HOVER':
-            return {mouseHover: action.payload};
+            return {...state, mouseHover: action.payload};
         default:
             return state;
     }
