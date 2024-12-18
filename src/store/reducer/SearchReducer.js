5a44673d897d75180f9b2c45d8cd9645dd84d5f2
Jae-Hong-Kim <newfly101@naver.com>
2024-12-18 15:39:55 +0900

    ➕ Reducer : state의 초기값 지정 및 default인 경우 state return해주기

---
+++ src/store/reducer/SearchReducer.js
@@ -0,0 +1,15 @@
+// const [searchResult, setSearchResult] = useState([]);
+// const [nextToken, setNextToken] = useState("");
+// const [mouseHover, setMouseHover] = useState(null);
+const initialState = {
+    searchResult: [],
+    nextToken: "",
+    mouseHover: null
+}
+
+const SearchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
