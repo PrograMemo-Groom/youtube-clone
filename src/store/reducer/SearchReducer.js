330f365b5a894f29241e32d710dc39a58b7ea0d9
Jae-Hong-Kim <newfly101@naver.com>
2024-12-18 15:47:10 +0900

    ➕ ActionType 별 상태값 변경 로직 설정

--- src/store/reducer/SearchReducer.js
+++ src/store/reducer/SearchReducer.js
@@ -9,7 +9,19 @@ const initialState = {
 
 const SearchReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'SET_SEARCH':
+            return {...state, searchResult: action.payload};
+        case 'SET_SEARCH_ERROR':
+            return {...state, error: action.payload};
+        case 'SET_NEXT_TOKEN':
+            return {...state, nextToken: action.payload};
+        case 'SET_NEXT_TOKEN_ERROR':
+            return {...state, error: null};
+        case 'SET_MOUSE_HOVER':
+            return {mouseHover: action.payload};
         default:
             return state;
     }
 }
+
+export default SearchReducer;
