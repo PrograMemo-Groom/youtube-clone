3b3b23e273c74fc5ebcc652d8580bd705c09e5cf
Jae-Hong-Kim <newfly101@naver.com>
2024-12-18 15:49:25 +0900

    ➕ index.js 에서 store 주입

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {GOOGLE_CLIENT_ID} from "./config/config";
+import {Provider} from "react-redux";
+import store from "./store/Store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
     </GoogleOAuthProvider>
 );
