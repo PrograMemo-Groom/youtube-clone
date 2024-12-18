02efaed88fcaf0803816c422ac08a98a05ffb38e
happygirlll <lbm000314@gmail.com>
2024-12-18 23:52:41 +0900

    💄사이드바 클릭 시 링크 연결

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -38,6 +38,10 @@ const Sidebar = ({ isExpanded }) => {
         });
     }
 
+    const handleExternalLink = (url) => {
+        window.location.href = url; // 외부 링크로 현재 페이지에서 이동
+    };
+
     return (
         <div className={`${styles.container} ${isExpanded ? styles.expanded : ''}`}
              onMouseEnter={() => isExpanded && setIsHovered(true)}
@@ -116,32 +120,38 @@ const Sidebar = ({ isExpanded }) => {
                             onClick={() => handleGoto('/myPage', 'myPage')}
                             style={{ cursor: 'pointer' }}>
                                 내 페이지 &nbsp;＞ </h3>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/history')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/history.svg`} alt="시청 기록"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>시청 기록</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/playlists')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/playlist.svg`} alt="재생목록"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>재생목록</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://studio.youtube.com/channel/UCQi6LBF0v9I-Bi3UT9uBVYA/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/videos.svg`} alt="내 동영상"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>내 동영상</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/courses')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/learning.svg`} alt="내 학습 프로그램"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>내 학습 프로그램</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/playlist?list=WL')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/later.svg`} alt="나중에 볼 동영상"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>나중에 볼 동영상</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/playlist?list=LL')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/liked.svg`} alt="좋아요"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>좋아요 표시한 동영상</span>
@@ -153,47 +163,56 @@ const Sidebar = ({ isExpanded }) => {
                     {/* 탐색 */}
                     <div className={styles.menuSection}>
                         <h3 className={styles.sectionTitle}>탐색</h3>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/trending.svg`} alt="인기 급상승"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>인기 급상승</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/channel/UCkYQyvc_i9hXEo4xic9Hh2g')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/shop.svg`} alt="쇼핑"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>쇼핑</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/music.svg`} alt="음악"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>음악</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/storefront?bp=ogUCKAU%3D')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/movie.svg`} alt="영화"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>영화</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/live.svg`} alt="실시간"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>실시간</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/gaming')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/game.svg`} alt="게임"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>게임</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/channel/UCEgdi0XIXXZ-qJOFPf4JSKw')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/sport.svg`} alt="스포츠"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>스포츠</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/feed/courses_destination')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/learning.svg`} alt="학습 프로그램"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>학습 프로그램</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/podcasts')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/podcast.svg`} alt="팟캐스트"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>팟캐스트</span>
@@ -204,22 +223,26 @@ const Sidebar = ({ isExpanded }) => {
                     {/* YouTube 더보기 섹션 */}
                     <div className={styles.menuSection}>
                         <h3 className={styles.sectionTitle}>YouTube 더보기</h3>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/premium')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/logo.svg`}
                                  alt="YouTube Premium" className={styles.icon}/>
                             <span className={styles.menuName}>YouTube Premium</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://studio.youtube.com/channel/UCQi6LBF0v9I-Bi3UT9uBVYA')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/logo.svg`} alt="YouTube 스튜디오"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>YouTube 스튜디오</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/musicpremium?ybp=ShUIBhIRdW5saW1pdGVkLUItbXVzaWPgAQE%253D')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/logo.svg`} alt="YouTube Music"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>YouTube Music</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtubekids.com/?source=youtube_web')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/logo.svg`} alt="YouTube Kids"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>YouTube Kids</span>
@@ -230,22 +253,26 @@ const Sidebar = ({ isExpanded }) => {
 
                     {/* 설정 섹션 */}
                     <div className={styles.menuSection}>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/account')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/settings.svg`} alt="설정"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>설정</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://www.youtube.com/reporthistory')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/flag.svg`} alt="신고 기록"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>신고 기록</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://support.google.com/youtube/?hl=ko#topic=9257498')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/help.svg`} alt="고객센터"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>고객센터</span>
                         </div>
-                        <div className={styles.menuItem}>
+                        <div className={styles.menuItem}
+                             onClick={() => handleExternalLink('https://support.google.com/youtube/answer/4347644?hl=ko&co=GENIE.Platform%3DDesktop')}>
                             <img src={`${process.env.PUBLIC_URL}/assets/sidebar/feedback.svg`} alt="의견 보내기"
                                  className={styles.icon}/>
                             <span className={styles.menuName}>의견 보내기</span>
