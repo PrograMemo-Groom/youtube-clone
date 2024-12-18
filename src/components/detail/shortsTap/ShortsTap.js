816edc29f5ce75f2708d77697d9764071fc6c4e0
saaad99 <ehdgnstla@naver.com>
2024-12-18 12:35:00 +0900

    :fire: 불필요한 import 삭제

--- src/components/detail/shortsTap/ShortsTap.js
+++ src/components/detail/shortsTap/ShortsTap.js
@@ -1,8 +1,5 @@
 import React, {useContext, useEffect, useRef, useState} from "react";
-import formatViewerCount from "../../../utils/formatViewerCount";
 import "./ShortsTap.css";
-import {ThemeContext} from "../../context/context";
-import {getStyle} from "../themes/useThemeStyles";
 import {fetchShortsVideos} from "../../../utils/fetchShortsVideos.js";
 import useNavigation from "../../../hooks/useNavigation.js";
 import ShortsVideos from "./shortsVideo/ShortsVideos.js";
