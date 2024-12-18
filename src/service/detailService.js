bc30a0a23790e379530ecf0eb6c93031b9495d94
saaad99 <ehdgnstla@naver.com>
2024-12-18 21:48:14 +0900

    :recycle: Details 전용 서비스 생성

---
+++ src/service/detailService.js
@@ -0,0 +1,23 @@
+import instance from "../api/api";
+import requests from "../api/endpoint";
+
+const tag = "[Service]";
+export const fetchVideoDetails = async (videoId) => {
+    if (!videoId) return;
+
+    try {
+        const response = await instance.get(requests.fetchPopularVideos, {
+            params: {
+                part: "snippet,statistics,contentDetails",
+                // chart: "mostPopular",
+                regionCode: "KR",
+                maxResults: 1,
+                id: videoId,
+            },
+        });
+
+        return {videoData: response.data.items[0], channelId: response.data.items[0].snippet.channelId}
+    } catch (e) {
+        console.log(tag, "PopularVideos can't get response data", e);
+    }
+};
