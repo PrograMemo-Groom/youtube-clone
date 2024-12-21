31df16ee0f72043cb8b0b8fb6b1298c9bbf4649b
happygirlll <lbm000314@gmail.com>
2024-12-21 13:47:24 +0900

    ♻️메인비디오에 있는 드랍다운메뉴 분리된 컴포넌트 불러오기

--- src/components/main/videos/MainVideos.js
+++ src/components/main/videos/MainVideos.js
@@ -3,7 +3,7 @@ import styles from "./MainVideos.module.css";
 import useNavigation from "../../../hooks/useNavigation";
 import { useSelector, useDispatch } from "react-redux";
 import {fetchVideos} from "../../../store/reducer/MainReducer";
-
+import DropdownMenu from "../../dropdownMenu/DropdownMenu";
 const MainVideos = ({ fetchFunction }) => {
     const scrollPositionRef = useRef(0); // 스크롤 위치 추적
 
@@ -123,75 +123,7 @@ const MainVideos = ({ fetchFunction }) => {
                                             onClick={() => toggleDropdown(video.videoId)}
                                         />
                                         {openDropdown === video.videoId && (
-                                            <div className={styles.dropdownMenu}>
-                                                <ul>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/playlist.svg`}
-                                                            alt="현재 재생목록에 추가"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        현재 재생목록에 추가
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/clock.svg`}
-                                                            alt="나중에 볼 동영상에 저장"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        나중에 볼 동영상에 저장
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/bookmark.svg`}
-                                                            alt="재생목록에 저장"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        재생목록에 저장
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/download.svg`}
-                                                            alt="오프라인 저장"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        오프라인 저장
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/share.svg`}
-                                                            alt="공유"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        공유
-                                                    </li>
-                                                    <hr className={styles.menuDivider}/>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/wrong.svg`}
-                                                            alt="관심 없음"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        관심 없음
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/no.svg`}
-                                                            alt="채널 추천 안함"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        채널 추천 안함
-                                                    </li>
-                                                    <li>
-                                                        <img
-                                                            src={`${process.env.PUBLIC_URL}/assets/videoMore/flag.svg`}
-                                                            alt="신고"
-                                                            className={styles.menuIcon}
-                                                        />
-                                                        신고
-                                                    </li>
-                                                </ul>
-                                            </div>
+                                            <DropdownMenu />
                                         )}
                                     </div>
                                 </div>
